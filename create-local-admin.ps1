a2af75f6599c9a4700ccb413b5119f7ce2b19dff
Jamie Lee <106612294+lokyeelee@users.noreply.github.com>
2026-06-29 14:22:54 +0800

    fixed typo

--- create-local-admin.ps1
+++ create-local-admin.ps1
@@ -1,3 +1,3 @@
 If ($null -eq (Get-LocalUser -Name "lapsadmin")){
-  New-LocalUser -AccountNeverExpires:$true -Password ( ConvertTo-SecureString -AsPlainText -Force 'TempPassword123!') -Name 'lapsadmin2' | Add-LocalGroupMember -Group administrators
+  New-LocalUser -AccountNeverExpires:$true -Password ( ConvertTo-SecureString -AsPlainText -Force 'TempPassword123!') -Name 'lapsadmin' | Add-LocalGroupMember -Group administrators
 }
